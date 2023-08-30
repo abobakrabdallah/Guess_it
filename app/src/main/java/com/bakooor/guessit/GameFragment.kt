22cbfe41db7b4eec133a40932e86d59de5363646
abobakr <bakoorabdallah80@gmail.com>
2023-08-30 15:12:17 +0300

    commit 3

--- app/src/main/java/com/bakooor/guessit/GameFragment.kt
+++ app/src/main/java/com/bakooor/guessit/GameFragment.kt
@@ -1,12 +1,13 @@
 package com.bakooor.guessit
 
+import android.accounts.AccountManager.get
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.*
+import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.navigation.fragment.findNavController
 import com.bakooor.guessit.databinding.FragmentGameBinding
 
@@ -14,9 +15,9 @@ class GameFragment : Fragment() {
 
     private lateinit var viewModel: GameViewModelFragment
     private lateinit var binding: FragmentGameBinding
-    private lateinit var movieList: MutableList<String>
-    private var word = ""
-    private var score = 0
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,75 +25,34 @@ class GameFragment : Fragment() {
     ): View {
         binding = FragmentGameBinding.inflate(inflater, container, false)
 
-        viewModel = ViewModelProvider(this)[GameViewModelFragment::class.java]
-        resetList()
-        nextWord()
-        updateWord()
-        updateScore()
 
-        binding.b2.setOnClickListener { onCorrect() }
-        binding.b3.setOnClickListener { onSkip() }
+        viewModel = ViewModelProvider(this)[GameViewModelFragment::class.java]
+
+        viewModel.score.observe(this, Observer{ newScore ->
+            binding.tvscore.text = newScore.toString()
+        })
+
+
+        binding.b2.setOnClickListener { viewModel.onCorrect()
+            updateWord()
+            }
+        binding.b3.setOnClickListener { viewModel.onSkip()
+            updateWord()
+            }
         return binding.root
     }
 
-    private fun resetList() {
-        movieList = mutableListOf(
-            "The Shawshank Redemption",
-            "The Godfather",
-            "The Dark Knight",
-            "Taxi Driver",
-            "Green Book",
-            "There Will Be Blood",
-            "Casino",
-            "Shutter Island",
-            "A Beautiful Mind",
-            "No Country for Old Men",
-            "Prisoners",
-            "The Hunt",
-            "Joker",
-            "Django Unchained",
-            "Memento",
-            "The Prestige",
-            "Whiplash",
-            "The Departed",
-            "The Pianist",
-            "Goodfellas",
-        )
-        movieList.shuffle()
-    }
+
 
     private fun gameFinished() {
-        findNavController().navigate(GameFragmentDirections.actionGameFragmentToScoreFragment(score))
+        findNavController().navigate(GameFragmentDirections.actionGameFragmentToScoreFragment(viewModel.score.value ?:0))
     }
-
-    private fun nextWord() {
-        if (movieList.isEmpty()) {
-            gameFinished()
-        } else {
-            word = movieList.removeAt(0)
-        }
-        updateScore()
-        updateWord()
-    }
-
-    private fun onCorrect() {
-        score++
-        nextWord()
-    }
-
-    private fun onSkip() {
-        score--
-        nextWord()
-    }
-
 
     private fun updateWord() {
-        binding.tvWord.text = word
+        binding.tvWord.text = viewModel.word
     }
 
-    private fun updateScore() {
-        binding.tvscore.text = score.toString()
-    }
+
 
 
 }
--- app/src/main/java/com/bakooor/guessit/GameViewModelFragment.kt
+++ app/src/main/java/com/bakooor/guessit/GameViewModelFragment.kt
@@ -1,15 +1,66 @@
 package com.bakooor.guessit
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModelFragment: ViewModel() {
+    lateinit var movieList: MutableList<String>
+    var word = ""
+    private val _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
     init{
         Log.i("GameViewModelFragment","okkkkkkkkkkkkkkkkkk")
+        resetList()
+        nextWord()
+        _score.value = 0
     }
+    private fun resetList() {
+        movieList = mutableListOf(
+            "The Shawshank Redemption",
+            "The Godfather",
+            "The Dark Knight",
+            "Taxi Driver",
+            "Green Book",
+            "There Will Be Blood",
+            "Casino",
+            "Shutter Island",
+            "A Beautiful Mind",
+            "No Country for Old Men",
+            "Prisoners",
+            "The Hunt",
+            "Joker",
+            "Django Unchained",
+            "Memento",
+            "The Prestige",
+            "Whiplash",
+            "The Departed",
+            "The Pianist",
+            "Goodfellas",
+        )
+        movieList.shuffle()
+    }
+    private fun nextWord() {
+        if (movieList.isEmpty()) {
+            //gameFinished()
+        } else {
+            word = movieList.removeAt(0)
+        }
+        }
+        fun onCorrect() {
+            _score.value = (_score.value)?.plus(1)
+            nextWord()
+        }
 
-    override fun onCleared() {
+        fun onSkip() {
+            _score.value = (_score.value)?.minus(1)
+            nextWord()
+        }
+        override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModelFragment", "kfkfkfkfkfkfkkkfkkfkkfkfkfkkfkfff")
     }
+
 }
